838ae731c2aa11e38d343e72b2f5ebd1afeb9bc6
boydpelley <boydjrpelley@gmail.com>
2023-12-26 17:24:55 -0400

    Created a function that renders a grid on the left side of the screen

--- main.c
+++ main.c
@@ -22,6 +22,28 @@ typedef struct
 
 } paint_state;
 
+void init_palette(paint_state *paint_state)
+{
+    // Initialize palette background
+    paint_state->p.background = (SDL_Rect){480, 0, 160, 480};
+
+    // Initialize colours
+    paint_state->p.colours[0] = (colours){490, 90, 60, 60, 255, 0, 0, 255};
+    paint_state->p.colours[1] = (colours){570, 90, 60, 60, 0, 255, 0, 255};
+
+    paint_state->p.colours[2] = (colours){490, 170, 60, 60, 0, 0, 255, 255};
+    paint_state->p.colours[3] = (colours){570, 170, 60, 60, 255, 255, 0, 255};
+
+    paint_state->p.colours[4] = (colours){490, 250, 60, 60, 255, 0, 255, 255};
+    paint_state->p.colours[5] = (colours){570, 250, 60, 60, 0, 255, 255, 255};
+
+    paint_state->p.colours[6] = (colours){490, 330, 60, 60, 0, 0, 0, 255};
+    paint_state->p.colours[7] = (colours){570, 330, 60, 60, 255, 255, 255, 255};
+
+    // Initialize save button
+    paint_state->p.save_btn = (SDL_Rect){490, 10, 140, 60};
+}
+
 short process_events(SDL_Window *window)
 {
     SDL_Event event;
@@ -85,6 +107,21 @@ void draw_palette(SDL_Renderer *renderer, palette *palette)
     }
 }
 
+void draw_grid(SDL_Renderer *renderer, int x, int y, int w, int h, int size)
+{
+    SDL_SetRenderDrawColor(renderer, 61, 61, 61, 255);
+
+    for (int i = x + size; i < x + w; i += size)
+    {
+        SDL_RenderDrawLine(renderer, i, y, i, y + h);
+    }
+
+    for (int i = y + size; i < y + h; i += size)
+    {
+        SDL_RenderDrawLine(renderer, x, i, x + w, i);
+    }
+}
+
 void render_screen(SDL_Renderer *renderer, paint_state *paint_state)
 {
     // Render a white screen
@@ -92,32 +129,16 @@ void render_screen(SDL_Renderer *renderer, paint_state *paint_state)
 
     SDL_RenderClear(renderer);
 
+    // Draw palette on right side of screen
     draw_palette(renderer, &paint_state->p);
+
+    // Draw grid on left side of screen
+    draw_grid(renderer, 0, 0, 480, 480, 20);
 
     SDL_RenderPresent(renderer);
 }
 
-void init_palette(paint_state *paint_state)
-{
-    // Initialize palette background
-    paint_state->p.background = (SDL_Rect){480, 0, 160, 480};
 
-    // Initialize colours
-    paint_state->p.colours[0] = (colours){490, 90, 60, 60, 255, 0, 0, 255};
-    paint_state->p.colours[1] = (colours){570, 90, 60, 60, 0, 255, 0, 255};
-
-    paint_state->p.colours[2] = (colours){490, 170, 60, 60, 0, 0, 255, 255};
-    paint_state->p.colours[3] = (colours){570, 170, 60, 60, 255, 255, 0, 255};
-
-    paint_state->p.colours[4] = (colours){490, 250, 60, 60, 255, 0, 255, 255};
-    paint_state->p.colours[5] = (colours){570, 250, 60, 60, 0, 255, 255, 255};
-
-    paint_state->p.colours[6] = (colours){490, 330, 60, 60, 0, 0, 0, 255};
-    paint_state->p.colours[7] = (colours){570, 330, 60, 60, 255, 255, 255, 255};
-
-    // Initialize save button
-    paint_state->p.save_btn = (SDL_Rect){490, 10, 140, 60};
-}
 
 int main(int argc, char *argv[])
 {
